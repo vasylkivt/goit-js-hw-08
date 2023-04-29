3e6deae00d6b740a233d78df7625c35e3137bafe
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-04-29 12:00:02 +0300

    update_tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,57 @@
 import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
-const refs = {
-  email: feedbackForm.querySelector('input[name="email"]'),
-  message: feedbackForm.querySelector('textarea[name="message"]'),
-};
-const { email, message } = refs;
-
 const LOCAL_STORAGE_DATA = 'feedback-form-state';
-let formData = {};
 
-email.setAttribute('required', '');
-message.setAttribute('required', '');
-
-const saveInputData = ({ target }) => {
-  const { name, value } = target;
-  formData[name] = value;
-  localStorage.setItem(LOCAL_STORAGE_DATA, JSON.stringify(formData));
-};
-
-const updateInputData = () => {
-  const data = JSON.parse(localStorage.getItem(LOCAL_STORAGE_DATA) || '{}');
-
-  email.value = data.email || '';
-  formData.email = data.email || '';
-
-  message.value = data.message || '';
-  formData.message = data.message || '';
-};
-
-const formSubmit = e => {
-  e.preventDefault();
-  localStorage.removeItem(LOCAL_STORAGE_DATA);
-
-  console.log(formData);
-  e.currentTarget.reset();
-  formData = {};
-};
+feedbackForm.elements.email.setAttribute('required', '');
+feedbackForm.elements.message.setAttribute('required', '');
 
 updateInputData();
 
 feedbackForm.addEventListener('submit', formSubmit);
 feedbackForm.addEventListener('input', throttle(saveInputData, 500));
+
+//?Ця функція отримує подію evt від обробника введення даних форми. Вона отримує збережені дані з локального сховища, парсить їх в об'єкт або створює новий порожній об'єкт, додає значення введеного поля до об'єкту збережених даних за ключем evt.target.name, а потім зберігає оновлені дані в локальному сховищі у форматі JSON.
+function saveInputData(evt) {
+  // Отримуємо збережені дані з локального сховища
+  let persistedData = localStorage.getItem(LOCAL_STORAGE_DATA);
+
+  // Перевіряємо, чи є збережені дані і парсимо їх з формату JSON в об'єкт, або створимо новий об'єкт
+  persistedData = persistedData ? JSON.parse(persistedData) : {};
+
+  // Зберігаємо значення введених даних для поточного елемента форми в об'єкт збережених даних
+  persistedData[evt.target.name] = evt.target.value;
+
+  // Зберігаємо оновлені дані в локальному сховищі у форматі JSON
+  localStorage.setItem(LOCAL_STORAGE_DATA, JSON.stringify(persistedData));
+}
+
+//?Ця функція отримує збережені дані з локального сховища за ключем LOCAL_STORAGE_DATA. Якщо дані існують, вони парсяться з формату JSON у вигляді об'єкта. Потім функція проходить по кожній парі ключ-значення в об'єкті збережених даних і встановлює значення поля форми відповідно до ключа name.
+function updateInputData() {
+  let persistedData = localStorage.getItem(LOCAL_STORAGE_DATA);
+
+  if (persistedData) {
+    persistedData = JSON.parse(persistedData);
+    Object.entries(persistedData).forEach(([name, value]) => {
+      feedbackForm.elements[name].value = value;
+    });
+  }
+}
+
+//? Ця функція виконується під час подання форми. Вона перешкоджає стандартному поведінці форми за допомогою e.preventDefault(). Потім вона видаляє збережені дані з локального сховища, і обробляє потрібні дані
+function formSubmit(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(LOCAL_STORAGE_DATA);
+
+  const formDataObj = {};
+  const formData = new FormData(feedbackForm);
+
+  formData.forEach((value, key) => {
+    console.log(key, value)
+    formDataObj[key] = value;
+  });
+
+  console.log('Дані з форми:', formDataObj);
+
+  evt.currentTarget.reset();
+}
